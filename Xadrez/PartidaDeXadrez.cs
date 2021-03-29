602a7e56cdcd7151e6caa66a5c1fee2f8f95d699
Wesley Oliveira <wesley.soliv@gmail.com>
2021-03-29 20:55:21 -0300

    Jogada especial, promoção

--- Xadrez/PartidaDeXadrez.cs
+++ Xadrez/PartidaDeXadrez.cs
@@ -137,6 +137,22 @@ namespace Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque");
             }
+
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial Promocao
+
+            if(p is Peao)
+            {
+                if(p.cor == Cor.Branca && destino.linha==0 || (p.cor == Cor.Preta && destino.linha == 7){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -156,8 +172,7 @@ namespace Xadrez
                 mudaJogador();
 
             }
-
-            Peca p = tab.peca(destino);
+           
             // #jogadaespecial En Passant
             if(p is Peao && (destino.linha == origem.linha-2 || destino.linha == origem.linha + 2))
             {
